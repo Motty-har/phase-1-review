e92f4bff40f104dffaa22d47f9442af0198f1440
Motty-har <harligmotty@gmail.com>
2023-06-03 22:18:29 -0400

    fixed post submitform event listener. first created object to hold values from the submit form

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const submitForm = document.getElementById('post')
-submitForm.addEventListener("submit", () => console.log("youve been clicked"))
+submitForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const firstName = event.target[0].value
+    const lastName = event.target[1].value
+    const goals = event.target[2].value
+    const assists = event.target[3].value
+    const playerStats = {
+        firstname: firstName,
+        lastname: lastName,
+        goals: goals,
+        assists: assists
+}
+
+}
+    
+
+    
+
+)
+/*fetch('http://localhost:3000/player',{   
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify()
+})
+.then(response => response.json())
+.then(JSON => console.log('hello') )
+)*/
